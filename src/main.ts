4ec8a082abef8fe9e6ade237e30e282108e21fac
Adriano Ribeiro <adriano.ribeiro@binddocs.com>
2025-04-15 10:29:45 -0300

    feat: crud :sparkles: - crud completo de users

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,16 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  
+
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT') || 3000;
-  
+  const port = configService.get<number>('PORT') || 3000;
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
